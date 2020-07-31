f9eb43572d281b6a5068227bc9ec67566b07e425
Mare <sevilamare@gmail.com>
2020-07-31 16:53:13 -0300

    first version

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-let turn = 0;
+const winCondition = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
+]
 
 const getMove = () => {
-  console.log(window.event.currentTarget.id);
+  const idx = window.event.currentTarget.id
+
+  // TODO: logic for each player play one time
+  const sel = document.getElementById(`${idx}`)
+  sel.innerHTML = 'X'
+  gameBoard[idx - 1] = 'X'
+  console.log(gameBoard)
+
+  win(winCondition, gameBoard)
 };
 
+
+const win = (winArray, boardArray) => {
+  console.log("START CHECKING")
+
+  for (let count = 0; count < winArray.length; count += 1) {
+    if ( winCondition[count].every( index => gameBoard[index] === 'X') ) {
+      console.log('WIN WIN')
+    }
+  }
+};
+
+
+
+
 document.querySelectorAll('.block').forEach(item => {
-  item.addEventListener('click',getMove);
+  item.addEventListener('click', getMove);
 })
+
+
